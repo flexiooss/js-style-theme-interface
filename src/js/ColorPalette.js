6f708e2b7ec5ef81a2a9d38d8483a2cf67d8ef5e
Basheitan <antoine@flexio.fr>
2020-03-05 10:26:35 +0100

    add color palette

---
+++ src/js/ColorPalette.js
@@ -0,0 +1,88 @@
+/**
+ * @interface
+ */
+export class ColorPalette {
+  /**
+   * @return {string}
+   */
+  white() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  light() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  dark() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  muted() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  primary() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  secondary() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  success() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  info() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  focus() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  warning() {
+    throw new Error('should be override')
+  }
+
+  /**
+   * @return {string}
+   */
+  danger() {
+    throw new Error('should be override')
+  }
+}
+
+/**
+ *
+ * @param instance
+ * @return {boolean}
+ */
+export const isColorPalette = (instance) => instance instanceof ColorPalette
